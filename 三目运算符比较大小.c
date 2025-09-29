2777aa03b588b8d935cb4328f1bcb06f24654d9e
CNGDLJH <2283581384@qq.com>
2025-09-29 21:42:31 +0800

    v1

---
+++ 三目运算符比较大小.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	scanf("%d" "%d" "%d",&a,&b,&c);
+	int max = a > (b > c ? b : c) ? a : (b > c ? b : c);
+	int min = a < (b < c ? b : c) ? a : (b < c ? b : c);
+	int med = a+b+c-max-min;
+	printf("%d %d %d",max,med,min);
+	return 0;
+}
